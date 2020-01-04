8ded5a2e0b188bbcba4127df5502b45fbf3d6deb
VincentLabor <vlabor95@gmail.com>
2020-01-04 15:50:06 -0800

    Added useUnifiedtopology to prevent deprecation

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+   await mongoose.connect(db, {
+      //These are added in case of errors that may appear.
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true 
+    });
+    console.log("Mongodb is connected");
+  } catch (error) {
+    console.log(error.message);
+    ProcessingInstruction.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const connectDB = require('./config/db');
 
+connectDB();
 
-app.get('/', (req,res) =>{
-    res.json({msg: "Welcome to the spokenSpiel API"});
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the spokenSpiel API" });
+});
 
 //These routes will then require module.exports = router; on each route file.
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>console.log(`The server on Port ${PORT} is now online`));
-
+app.listen(PORT, () => console.log(`The server on Port ${PORT} is now online`));
