2677640e14d678fb39ee38c119ace7f0dab90230
VincentLabor <vlabor95@gmail.com>
2020-01-03 22:01:04 -0800

    First commit

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//@route    POST /api/users
+//@desc     Register users
+//@access   Public - To register and become a user. 
+router.post("/", (req,res)=>{
+    
+});
