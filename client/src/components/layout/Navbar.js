e5921193ad5290df9a62c4c07a420f9e10a333b9
VincentLabor <vlabor95@gmail.com>
2020-01-21 16:45:01 -0800

    Added React

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return <nav className="nav">
+      <ul>
+          <li>Login</li>
+          <li>Register</li>
+          <li></li>
+          </ul>
+  </nav>;
+};
+
+export default Navbar;
